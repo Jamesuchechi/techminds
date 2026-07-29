bed80d70a90162d0bc669d530a4c5fc44ddc3e21
James Uchechi <popejamesjedidiah@gmail.com>
2026-07-29 12:53:44 +0100

    Add HTML files for Bwari Kitchen website and JavaScript examples for array manipulation

---
+++ Webdev4/Classes/Array/classwork.js
@@ -0,0 +1,20 @@
+let student = ["shelia", "Aaron","James", "kelvinson", "mary"];
+console.log(student);
+
+student.push("Rachael");
+console.log(student);
+
+student.splice(2, 1, "Hilary");
+console.log(student);
+
+student.pop("mary");
+console.log(student);
+
+student.shift("shelia");
+console.log(student);
+
+student.unshift("Mercy");
+console.log(student);
+
+
+
---
+++ Webdev4/Classes/Array/script.js
@@ -0,0 +1,19 @@
+// Array in javascript //
+let fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"];
+console.log(fruits[3]);
+
+fruits[4] = "Pineapple";
+
+console.log(fruits);
+
+fruits.push("pear");
+console.log(fruits);
+
+fruits.pop("");
+console.log(fruits);
+
+
+const student = ["mary", "john", "shelia", "kelvinson", "hilary"];
+student.splice(2, 4);
+console.log(student);
+
