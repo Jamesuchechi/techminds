49827f8050aacb461ec53aeb8bfba680ba442b56
James Uchechi <popejamesjedidiah@gmail.com>
2026-07-29 14:04:12 +0100

    feat: create person object and implement property modification and deletion logic

---
+++ Webdev4/Classes/Object/main.js
@@ -0,0 +1,22 @@
+// object //
+let person = {
+    "name" : "mshelia",
+    "age" : 14,
+    "height" : 192,
+    "email" : "mshelia@gmail.com",
+    "gender" : "female",
+    "married" : false
+}
+console.log(person);
+
+person.age = 50;
+console.log(person);
+
+person.food = "rice";
+console.log (person);
+
+delete person.age;  
+console.log(person);
+
+
+
